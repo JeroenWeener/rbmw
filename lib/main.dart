ef062a8edc48acb2423679fc4d85bc294ed0b05c
Jeroen Weener <jweener1996@gmail.com>
2023-09-02 14:37:05 +0200

    Change page title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Redactie Bureau Marita Weener',
       theme: ThemeData(
         // This is the theme of your application.
         //
