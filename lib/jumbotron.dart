2cbceba1545a09055c227d7bbf908989f740479b
Jeroen Weener <jeroen.weener@baseflow.com>
2023-09-03 17:16:57 +0200

    Scale the picture

--- lib/jumbotron.dart
+++ lib/jumbotron.dart
@@ -9,20 +9,24 @@ class Jumbotron extends StatelessWidget {
     return ColoredBox(
       color: RbmwTheme.colorScheme.primaryContainer,
       child: Padding(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.symmetric(vertical: 32),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Column(
               children: [
-                Text(
-                  style: Theme.of(context).textTheme.headlineLarge,
-                  'Redactiebureau Marita Weener',
-                ),
-                const Text('Voor de puntjes op je i'),
+                // Text(
+                //   style: Theme.of(context).textTheme.headlineLarge,
+                //   'Redactiebureau Marita Weener',
+                // ),
+                // const Text('Voor de puntjes op je i'),
               ],
             ),
-            Image.asset('assets/90a441_5ae6a7c87f1c46319449274202991626~mv2.png'),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 2,
+              child: Image.asset(
+                  'assets/90a441_5ae6a7c87f1c46319449274202991626~mv2.png')
+            ),
           ],
         ),
       ),
