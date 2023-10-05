a71bbf4a960f341ce27d39ce88d29f814303cb80
Jeroen Weener <jweener1996@gmail.com>
2023-10-05 22:11:17 +0200

    Fix footer build error

--- lib/footer.dart
+++ lib/footer.dart
@@ -6,19 +6,26 @@ class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    final TextStyle? footerTextStyle =
-        Theme.of(context).textTheme.bodyMedium?.apply(color: colorScheme.onSecondary);
-    return ColoredBox(
-      color: colorScheme.secondary,
-      child: const Padding(
-        padding: EdgeInsets.all(40.0),
-        child: Column(
-          children: [
-            ContactDetail(icon: Icons.phone, text: '06 235 873 74'),
-            ContactDetail(icon: Icons.phone, text: '0592 769 056'),
-            ContactDetail(
-                icon: Icons.mail, text: 'info@redactiebureaumaritaweener.nl'),
-          ],
+
+    return SizedBox(
+      width: double.infinity,
+      child: ColoredBox(
+        color: colorScheme.secondary,
+        child: const Padding(
+          padding: EdgeInsets.symmetric(vertical: 40.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _ContactDetail(icon: Icons.phone, text: '06 235 873 74'),
+                _ContactDetail(icon: Icons.phone, text: '0592 769 056'),
+                _ContactDetail(
+                  icon: Icons.mail,
+                  text: 'info@redactiebureaumaritaweener.nl',
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -29,7 +36,6 @@ class _ContactDetail extends StatelessWidget {
   const _ContactDetail({
     required this.icon,
     required this.text,
-    super.key,
   });
 
   final IconData icon;
@@ -40,9 +46,11 @@ class _ContactDetail extends StatelessWidget {
     final onSecondary = Theme.of(context).colorScheme.onSecondary;
     final TextStyle? textStyle =
         Theme.of(context).textTheme.bodyMedium?.apply(color: onSecondary);
+
     return Padding(
       padding: const EdgeInsets.all(4.0),
       child: Row(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Icon(icon, color: onSecondary),
           const SizedBox(width: 16),
